639a0acbfa918ab669532230d0d0b8ec9ce570b4
Jing <jlin3627@gmail.com>
2020-03-18 23:11:04 -0400

    First Build for cmake
    Cmake hello world first build

---
+++ MyTutorialPractice/CMakeFiles/CMakeTutorial.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Software Projects/CMake Projects/tutorial/MyTutorialPractice/cmake_helloWorld.cpp" "D:/Software Projects/CMake Projects/tutorial/MyTutorialPractice/CMakeFiles/CMakeTutorial.dir/cmake_helloWorld.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ MyTutorialPractice/CMakeFiles/CMakeTutorial.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeTutorial.dir/cmake_helloWorld.cpp.obj"
+  "CMakeTutorial.exe"
+  "CMakeTutorial.exe.manifest"
+  "CMakeTutorial.pdb"
+  "libCMakeTutorial.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMakeTutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
