3a3b3f54cd474c4daae69394a4cf3bb44e160ae8
Jing <jlin3627@gmail.com>
2020-03-18 23:39:47 -0400

    Added mingw compile practice
    Add mingw compile practice

---
+++ Mingw Practice/helloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    /*if(arc < 2)
+    {
+        std::cout << "Usage: " << argv[0] << "number" << std::endl;
+        return 1;
+    }*/
+
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
