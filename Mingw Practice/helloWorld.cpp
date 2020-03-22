972b688171f816aa8c02e590be01918cc97f8d8d
Jing <jlin3627@gmail.com>
2020-03-21 22:49:47 -0400

    elemantary hw programming practice
    practice display

--- Mingw Practice/helloWorld.cpp
+++ Mingw Practice/helloWorld.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <string>
+
+
+
 
 int main(int argc, char* argv[])
 {
@@ -7,8 +11,21 @@ int main(int argc, char* argv[])
         std::cout << "Usage: " << argv[0] << "number" << std::endl;
         return 1;
     }*/
-
-    std::cout << "Hello World" << std::endl;
+    std::string colors[7] = {"R","O","Y","G","B","C","P"};
+    for(int i = 0; i<10 ; i ++)
+    {
+        for(int j = 0; j<7; j++)
+        {
+            std::cout<<"("<<j+i*7+1<<")";
+        }
+        std::cout<<std::endl;
+        for(int j = 0; j<7; j++)
+        {
+            std::cout<<"("<<colors[j]<<")";
+        }
+        std::cout<<std::endl;
+    }
+    
 
     return 0;
 }
